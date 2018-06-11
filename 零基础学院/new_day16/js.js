99c657fc626c084c9fd204c00bb5b19a74e8cf06
yuzmt <37643759+yuzmt@users.noreply.github.com>
2018-06-11 02:07:57 -0500

    Add files via upload

---
+++ 零基础学院/new_day16/js.js
@@ -0,0 +1,38 @@
+
+
+
+
+// document.getElementById("send-btn").onclick = function() {
+// 	document.getElementById("wrapper").innerHTML = 'hello' 
+// 	+ document.getElementById("name-input").value;
+// }
+
+// document.getElementById("add-btn").onclick = function() {
+// 	document.getElementById("result").innerHTML = document.getElementById
+// 	("first-number").value + document.getElementById
+// 	("second-number").value;
+// }
+
+// document.getElementById("minus-btn").onclick = function() {
+// 	document.getElementById("result").innerHTML = document.getElementById
+// 	("first-number").value - document.getElementById
+// 	("second-number").value;
+// }
+
+// document.getElementById("times-btn").onclick = function() {
+// 	document.getElementById("result").innerHTML = document.getElementById
+// 	("first-number").value * document.getElementById
+// 	("second-number").value;
+// }
+
+// document.getElementById("divide-btn").onclick = function() {
+// 	document.getElementById("result").innerHTML = document.getElementById
+// 	("first-number").value / document.getElementById
+// 	("second-number").value;
+// }
+
+
+document.getElementById("j_li").onclick = function() {
+	document.getElementById("lookme").innerHTML = '<br>聘聘袅袅十三余<br>豆蔻梢头二月初';
+
+}
